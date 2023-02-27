a974502f65b8d21fbb7b107102e93be12cd2db73
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2023-02-27 11:25:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
 //console.log('Hello World!');
 function encoder() {
 let frmInp = document.getElementById('message').value; 
-let inpArr = Array.from(frmInp, x => x.charCodeAt());
+let inpArr = Array.from(frmInp, x => x.codePointAt());
 //alert(inpArr);
 const cmap = {0:"z",1:"o",2:"t",3:"T",4:"f",5:"F",6:"s",7:"S",8:"e",9:"n"};
 let pkey = document.getElementById("key").value;
 //pkey = pkey.length == 10? ;
 //alert(pkey);
-let fstrng = "";  let nidf = "";
-if (pkey.length === 10) {
+let fstrng = "";  let nidf = ""; let nid = "";
+if (Array.from(pkey, x => x.codePointAt()).length === 10) {
   //alert(Array.from(pkey, x => x.charCodeAt()));
-  nidf = Math.max(...Array.from(pkey, x => x.charCodeAt())) + 1; //alert(String.fromCharCode(nidf));
-  nidf = String.fromCharCode(nidf);
+  console.log("Pkey: "+Array.from(pkey, x => x.codePointAt()));
+  nidf = Math.max(...Array.from(pkey, x => x.codePointAt())); //alert(String.fromCharCode(nidf));
+  console.log("Max: " + nidf);
+nidf++;
+console.log("INC: " + nidf);
+nid = nidf;
+  nidf = String.fromCodePoint(nidf);
+console.log("ID: " + nidf);
+
   fstrng = inpArr.toString().replaceAll(",",nidf);
   
 } else {
   fstrng = inpArr.toString().replaceAll(",","c");
 }
-
+//console.log("ID: "+nidf);
 let noupt="";
-if (pkey.length === 10) {
+if (Array.from(pkey, x => x.codePointAt()).length === 10) {
   for (let prop of fstrng) {
     if (prop != nidf) {
       noupt += pkey[prop]+cmap[prop];
+      //console.log(cmap[prop]);
     } else {
       noupt += prop;
     }
   }
-  
+  noupt = nidf + noupt + "0"; 
 } else {
   for (let prop of fstrng) {
     if (prop != "c") {
